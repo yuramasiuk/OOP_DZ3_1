6b73c5a75d698bf79486c2d8940751ebdf122350
yuriiqrono <yurii@qrono.dev>
2022-11-27 20:59:20 +0200

    first commit

---
+++ Converter/Converter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Converter
+    {
+        private double USD; //usd in 1 uah
+        private double EUR; //eur in 1 uah
+        public Converter(double usd, double eur)
+        {
+            USD = usd; EUR = eur;
+        }
+
+        public double UAHtoUSD(double uah)
+        {
+            return uah / USD;
+        }
+        public double UAHtoEUR(double uah)
+        {
+            return uah / EUR;
+        }
+        public double USDtoUAH(double usd)
+        {
+            return usd * USD;
+        }
+        public double EURtoUAH(double eur)
+        {
+            return eur * EUR;
+        }
+    }
+}
---
+++ Converter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter Conv = new Converter(36.92, 38.40);
+            while(true)
+            { 
+            Console.WriteLine("Choose the operation:\n 1 - EUR to UAH\n 2 - USD to UAH\n 3 - UAH to EUR\n 4 - UAH to USD");
+            int act = Convert.ToInt32(Console.ReadLine());
+                switch (act)
+                {
+                    case 1:
+                        Console.WriteLine("Enter EUR: ");
+                        double eur = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(Conv.EURtoUAH(eur));
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter USD: ");
+                        double usd = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(Conv.USDtoUAH(usd));
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("Enter UAH: ");
+                            double uah = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine(Conv.UAHtoEUR(uah));
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Enter UAH: ");
+                            double uah = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine(Conv.UAHtoUSD(uah));
+                            break;
+                        }
+                }
+                Console.WriteLine("If you want to continie, put 1. If not - put 0.");
+                int cont = Convert.ToInt32(Console.ReadLine());
+                if (cont == 0) break;
+
+            }
+        }
+    }
+}
